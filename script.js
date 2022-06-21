d5515af084899fb9dab35891edb872a528f8e427
Arif Aydoğan <aydoganarif@yahoo.com>
2022-06-22 01:34:11 +0300

    aa

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let name = prompt("Lütfen İsminizi Giriniz..");
+while (name == '' || name == null) {
+    name = prompt("Lütfen Geçerli Bir İsim Giriniz..");
+}
+document.querySelector('#myName').innerHTML = name;
+
+let gunler = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+let renkler = ["black", "gray", "white"];
+
+function tarihSaat() {
+    var d = new Date();
+    document.querySelector('#myDate').innerHTML = d.toLocaleTimeString() + " " +
+        gunler[d.getDay()];
+}
+
+tarihSaat();
+setInterval(tarihSaat, 111); 
